f07220c360a4167ec1a1fe18025537d39bafb44f
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2018-01-08 10:37:00 -0300

    Etapa 2: append Nuevas recetas en p de div callout-news

--- js/script-index.js
+++ js/script-index.js
@@ -3,9 +3,19 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 	$(".js-back").hide();
-	
+
+$(function printNews(){
+	$(".showNews").append('NUEVAS RECETAS')
+});
+
 
 });
+
+/*
+* Función que se encarga de pintar una noticia
+* dentro del p class showNews
+*/
+
 
 
 /*
